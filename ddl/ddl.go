25136286a0d68047691dcc6b279fa08a5b971e58
Weizhen Wang <wangweizhen@pingcap.com>
2022-02-22 16:04:22 +0800

    update

--- ddl/ddl.go
+++ ddl/ddl.go
@@ -415,7 +415,7 @@ func (d *ddl) Start(ctxPool *pools.ResourcePool) error {
 				return wk, nil
 			}
 			d.addIdxWorker = newDDLWorkerPool(pools.NewResourcePool(addIdxWorkerFunc, 10, 10, 3*time.Minute))
-			d.generalWorker = newDDLWorkerPool(pools.NewResourcePool(generalWorkerFunc, 10, 10, 0))
+			d.generalWorker = newDDLWorkerPool(pools.NewResourcePool(generalWorkerFunc, 300, 300, 0))
 			d.sessForAddDDL, _ = d.sessPool.get()
 			d.wg.Run(d.startDispatchLoop)
 		} else {
